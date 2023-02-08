c87c9b64c27a959e80e0e5a70556d09153d72b10
maxsvst <fioklin2015@mail.ru>
2023-02-08 15:24:17 +0300

    Стики хэдер и валидация имейла

--- src/ui/organisms/form/GetInTouchForm.js
+++ src/ui/organisms/form/GetInTouchForm.js
@@ -56,6 +56,10 @@ export default function GetInTouchForm() {
                 <Input
                   register={register("email", {
                     required: "Please fill this mandatory field",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+                      message: "Invalid email adress"
+                    }
                   })}
                   title="Your email "
                   value="name@example.com"
--- src/ui/organisms/header/MainPageNavbar.js
+++ src/ui/organisms/header/MainPageNavbar.js
@@ -5,15 +5,17 @@ import headerLogo from "../../../icons/main/header/headerLogo.svg";
 
 function MainPageNavbar() {
   return (
-    <div className="border-b border-[#E5E7EB] flex justify-between items-center h-[64px] px-4 sm:px-[39px] lg:px-[85px]">
-      <img src={headerLogo} alt={headerLogo} className="w-[35px] h-[35px]" />
-      <ul className="hidden sm:flex justify-between list-none w-[242px]">
-        <NavbarLink href="#main" title="Main" />
-        <NavbarLink href="#about" title="About" />
-        <NavbarLink href="#getInTouch" title="Get in touch" />
-      </ul>
-      <img src={headerMenu} alt={headerMenu} className="sm:hidden" />
-    </div>
+    <div className="sticky h-[64px] z-10 top-0 bg-[#FCFBFA]">
+      <div className="border-b border-[#E5E7EB] flex justify-between items-center h-[64px] px-4 sm:px-[39px] lg:px-[85px]">
+        <img src={headerLogo} alt={headerLogo} className="w-[35px] h-[35px]" />
+        <ul className="hidden sm:flex justify-between list-none w-[242px]">
+          <NavbarLink href="#main" title="Main" />
+          <NavbarLink href="#about" title="About" />
+          <NavbarLink href="#getInTouch" title="Get in touch" />
+        </ul>
+        <img src={headerMenu} alt={headerMenu} className="sm:hidden" />
+      </div>
+     </div>
   );
 }
 
